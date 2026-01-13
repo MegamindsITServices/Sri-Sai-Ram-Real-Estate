63912a8612beee0d50c767cf200e085b30cdab38
Roshan Yadav <yroshan504@gmail.com>
2026-01-13 16:00:28 +0530

    project details page changed and top project checkbox added

--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ app.use("/api/v1/wishlist", WishlistRoute);
 app.use("/api/v1/msg", MessageRoute);
 app.use("/api/v1/testimonial", upload.single("profileImage"), TestimonalRoute);
 //Routes End
-const port = 5431;
+const port = process.env.PORT || 5435;
 const connectDb = async () => {
   try {
     await mongoose.connect(`${process.env.MONGO_URI}`);
